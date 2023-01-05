14c5da04d8fbc7fe43b53a18a4906902a6de92dc
imanuel lombardo <imanuel.lombardo@hotmail.com>
2023-01-05 16:55:57 +0100

    Basic Operators in bash

---
+++ basicOperators.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+####################
+# Default variables
+####################
+
+COST_PINEAPPLE=50
+COST_BANANA=4
+COST_WATERMELON=23
+COST_BASKET=1
+
+############
+# To change
+############
+
+TOTAL=
+
+##########
+# Testing
+##########
+
+echo "Total Cost is $TOTAL"
